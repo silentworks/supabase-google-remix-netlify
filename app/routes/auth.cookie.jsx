1f833119f923e82afca1e234e6ca039fce8649ea
Andrew Smith <a.smith@silentworks.co.uk>
2023-11-12 23:50:23 +0000

    log headers

--- app/routes/auth.cookie.jsx
+++ app/routes/auth.cookie.jsx
@@ -5,10 +5,11 @@ export const loader = async ({ request }) => {
   const chunks = ['hello-world.and.more.to.come', 'there.will.be.more']
   chunks.forEach(async (chunk, i) => {
     const cookie = createCookie(`my-cookie.${i}`, {
-      sameSite: "lax"
+      sameSite: "lax",
+      maxAge: 604_800
     })
     headers.append('Set-Cookie', await cookie.serialize(chunk));
   })
-    
+  console.log({ headers })
   return redirect('/', { headers });
 };
