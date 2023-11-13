e51ab55a0acdcaeee7b26871b5f180f142f4e72a
Andrew Smith <a.smith@silentworks.co.uk>
2023-11-13 01:18:30 +0000

    map headers

--- app/routes/auth.cookies.jsx
+++ app/routes/auth.cookies.jsx
@@ -3,14 +3,19 @@ import { serialize } from '@supabase/ssr';
 
 export const loader = async ({ request }) => {
   const responseHeaders = new Headers();
+  const cookieHeaders = []
   const chunks = ['hello-world.and.more.to.come', 'there.will.be.more']
   chunks.forEach(async (chunk, i) => {
-    responseHeaders.append('Set-Cookie', serialize(`my-cookie.${i}`, chunk, {
+    cookieHeaders.push(serialize(`my-cookie.${i}`, chunk, {
       sameSite: "lax",
       maxAge: 604_800
-    }));
+    }))
+    // responseHeaders.append('Set-Cookie', serialize(`my-cookie.${i}`, chunk, {
+    //   sameSite: "lax",
+    //   maxAge: 604_800
+    // }));
   })
-  console.log({ ...Object.fromEntries(responseHeaders) })
+  console.log({ ...cookieHeaders })
   // return redirect('/', { 
   //   headers: {
   //     "Set-Cookie": serialize('my-test', "hello-world"),
@@ -21,6 +26,11 @@ export const loader = async ({ request }) => {
   responseHeaders.append('Location', '/')
   return new Response(null, {
     status: 302,
-    headers: responseHeaders,
+    headers: {
+      "Set-Cookie": serialize('my-test', "hello-world"),
+      // @ts-expect-error
+      "Set-Cookie": serialize('my-test-cookie.1', "hello-people"),
+      // "Set-Cookie": [...cookieHeaders]
+    },
   })
 };
