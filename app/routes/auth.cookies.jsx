6c601cb52be0ea6dda21b29b9dcf445b15bb9828
Andrew Smith <a.smith@silentworks.co.uk>
2023-11-13 00:55:34 +0000

    using response

--- app/routes/auth.cookies.jsx
+++ app/routes/auth.cookies.jsx
@@ -5,17 +5,22 @@ export const loader = async ({ request }) => {
   const responseHeaders = new Headers();
   const chunks = ['hello-world.and.more.to.come', 'there.will.be.more']
   chunks.forEach(async (chunk, i) => {
-    responseHeaders.append('Set-Cookie', serialize(`my-cookie.${i}`, chunk, {
+    responseHeaders.set('Set-Cookie', serialize(`my-cookie.${i}`, chunk, {
       sameSite: "lax",
       maxAge: 604_800
     }));
   })
   console.log({ ...Object.fromEntries(responseHeaders) })
-  return redirect('/', { 
-    headers: {
-      "Set-Cookie": serialize('my-test-cookie.0', "hello-world"),
-        // @ts-expect-error
-      "Set-Cookie": serialize('my-test-cookie.1', "hello-people"),
-    }
-  });
+  // return redirect('/', { 
+  //   headers: {
+  //     "Set-Cookie": serialize('my-test', "hello-world"),
+  //       // @ts-expect-error
+  //     "Set-Cookie": serialize('my-test-cookie.1', "hello-people"),
+  //   }
+  // });
+  responseHeaders.append('Location', '/')
+  return new Response('', {
+    status: 302,
+    headers: responseHeaders,
+  })
 };
