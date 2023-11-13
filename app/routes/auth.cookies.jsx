153ce2bfbd75e8cd27874748abd11a39e45e10d0
Andrew Smith <a.smith@silentworks.co.uk>
2023-11-13 00:23:04 +0000

    Add new cookies file

---
+++ app/routes/auth.cookies.jsx
@@ -0,0 +1,19 @@
+import { redirect, createCookie } from '@remix-run/node';
+import { serialize } from '@supabase/ssr';
+
+export const loader = async ({ request }) => {
+  const responseHeaders = new Headers();
+  const chunks = ['hello-world.and.more.to.come', 'there.will.be.more']
+  chunks.forEach(async (chunk, i) => {
+    responseHeaders.append('Set-Cookie', serialize(`my-cookie.${i}`, chunk, {
+      sameSite: "lax",
+      maxAge: 604_800
+    }));
+  })
+  console.log({ responseHeaders })
+  return redirect('/', { 
+    headers: {
+      ...Object.fromEntries(responseHeaders)
+    }
+  });
+};
