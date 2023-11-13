8198779a25f5b5d2bcbc867c9e764f278fdaf55c
Andrew Smith <a.smith@silentworks.co.uk>
2023-11-13 00:01:37 +0000

    using serialise from ssr

--- app/routes/auth.cookie.jsx
+++ app/routes/auth.cookie.jsx
@@ -1,14 +1,14 @@
 import { redirect, createCookie } from '@remix-run/node';
+import { serialize } from '@supabase/ssr';
 
 export const loader = async ({ request }) => {
   const headers = new Headers();
   const chunks = ['hello-world.and.more.to.come', 'there.will.be.more']
   chunks.forEach(async (chunk, i) => {
-    const cookie = createCookie(`my-cookie.${i}`, {
+    headers.append('Set-Cookie', serialize(`my-cookie.${i}`, chunk, {
       sameSite: "lax",
       maxAge: 604_800
-    })
-    headers.append('Set-Cookie', await cookie.serialize(chunk));
+    }));
   })
   console.log({ headers })
   return redirect('/', { headers });
